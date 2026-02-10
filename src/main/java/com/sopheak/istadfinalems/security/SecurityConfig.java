b020a298a7d7bc12e6ba00ae5885937a6d9a1a6f
Chheang Pheak <sop219151@gmail.com>
2026-02-11 00:33:59 +0700

    first day of section 5 implementaion

--- src/main/java/com/sopheak/istadfinalems/security/SecurityConfig.java
+++ src/main/java/com/sopheak/istadfinalems/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.sopheak.istadfinalems.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,18 +14,16 @@ import org.springframework.security.web.SecurityFilterChain;
 @RequiredArgsConstructor
 @EnableMethodSecurity
 public class SecurityConfig {
-    private final JwtConverterConfigure jwtConverterConfigure;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity)
             throws Exception{
         httpSecurity
                 .cors(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
-                // this for authentication and authorization
-                .authorizeHttpRequests(
-                        re->re.anyRequest().permitAll());
-        httpSecurity.oauth2ResourceServer(jwt->
-                jwt.jwt(convert->convert.jwtAuthenticationConverter(jwtConverterConfigure)));
+                .authorizeHttpRequests(re->re
+                        .anyRequest().authenticated());
+        httpSecurity.oauth2ResourceServer(jwt-> jwt.jwt(Customizer.withDefaults()));
         return httpSecurity.build();
     }
 }
